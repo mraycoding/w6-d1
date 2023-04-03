0eedd97b694fb943499adbcf8eefded52f369897
Max Ray <maxxray@yahoo.com>
2023-04-02 22:50:02 -0600

    commit

---
+++ app.js
@@ -0,0 +1,78 @@
+const storeItems = [
+    {
+        name: 'TV',
+        price: 800.00,
+        inStock: true,
+        details: '4K Ultra HD'
+    },
+    {
+        name: 'Game Console',
+        price: 300.00,
+        inStock: true,
+        details: 'The latest and greatest'
+    },
+    {
+        name: 'Laptop',
+        price: 1200.00,
+        inStock: true,
+        details: '16GB RAM 1TB SSD'
+    },
+    
+    {
+        name: 'Headphones',
+        price: 100.00,
+        inStock: true,
+        details: 'Clearest music to be heard'
+    },
+    {
+        name: 'Keyboard',
+        price: 100.00,
+        inStock: true,
+        details: 'Types for you'
+    },
+    {
+        name: 'HDMI Cord',
+        price: 100.00,
+        inStock: true,
+        details: 'HDMI to USB type C'
+    },
+    {
+        name: 'Monitor',
+        price: 300.00,
+        inStock: true,
+        details: '4K Ultra HD'
+    },
+    {
+        name: 'Speaker',
+        price: 200.00,
+        inStock: true,
+        details: 'Clearest music to be heard'
+    },
+    {
+        name: 'Video Game',
+        price: 60.00,
+        inStock: true,
+        details: 'Enjoy for hours'
+    },
+];
+
+
+
+const appendToMe = document.querySelector('#containers');
+storeItems.forEach(item => {
+  const paragraph = document.createElement('p');
+  const stockStatus = item.inStock ? 'inStock' : 'notInStock';
+  paragraph.classList.add(stockStatus);
+  paragraph.textContent = `${item.price}: $${item.name}: $${item.details}`;
+
+  
+  appendToMe.appendChild(paragraph);
+});
+
+
+
+const clickMe = document.querySelector('#clickMe');
+const contentContainer = document.querySelector('#josh');
+clickMe.addEventListener('click', () => {
+  contentContainer.classList.toggle('darkMode');
+});
